8b154d8f3d42d2a838d1b1c46a77ea5b6b82042d
Jan Boerman <janboerman95@gmail.com>
2022-11-29 12:23:44 +0100

    first commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+lazy val root = project.in(file("."))
+    .settings(
+        name := "AdventOfCode2022",
+        version := "0.1",
+        scalaVersion := "3.2.1",
+    )
+
---
+++ src/main/scala/day01/Day01.scala
@@ -0,0 +1,15 @@
+package day01
+
+import scala.io.Source;
+
+val source = Source.fromResource("day01.in")
+
+@main def main: Unit = {
+
+    val result1 = ???
+    println(result1)
+
+    val result2 = ???
+    println(result2)
+
+}
