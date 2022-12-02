b62eb7b76a4f0024cd9c0823b0e76b30204a61fe
Jan Boerman <janboerman95@gmail.com>
2022-12-02 10:08:04 +0100

    clean up day02

--- src/main/scala/day02/Day02.scala
+++ src/main/scala/day02/Day02.scala
@@ -3,35 +3,20 @@ package day02
 import scala.io.Source;
 
 val source = Source.fromResource("day02.in")
-val diff = 'X'.toInt - 'A'.toInt
 
-enum RPS extends java.lang.Enum[RPS]:
-    case Rock
-    case Paper
-    case Scissors
+enum RPS:
+    case Rock, Paper, Scissors
+enum Outcome:
+    case Win, Lose, Draw
 import RPS.*
-enum Outcome extends java.lang.Enum[Outcome]:
-    case Win
-    case Lose
-    case Draw
 import Outcome.*
 
-object Them:
-    def parse(x: String): Them = x match
-        case "A" => A
-        case "B" => B
-        case "C" => C
 enum Them(rps: RPS):
     case A extends Them(Rock)
     case B extends Them(Paper)
     case C extends Them(Scissors)
     def token: RPS = rps
 
-object Us:
-    def parse(x: String): Us = x match
-        case "X" => X
-        case "Y" => Y
-        case "Z" => Z
 enum Us(rps: RPS, oc: Outcome):
     case X extends Us(Rock, Lose)
     case Y extends Us(Paper, Draw)
@@ -41,26 +26,25 @@ enum Us(rps: RPS, oc: Outcome):
 
 type Instruction = (Them, Us)
 
-val input: List[Instruction] = source.getLines().map { case s"${them} ${us}" => (Them.parse(them), Us.parse(us))}.toList
+val input: List[Instruction] = source.getLines().map { case s"${them} ${us}" => (Them.valueOf(them), Us.valueOf(us))}.toList
 val checkArray: Array[RPS] = RPS.values ++ RPS.values
 
-def outcomeScore(them: RPS, us: RPS): Int = {
+def outcomeScore(them: RPS, us: RPS): Int =
     for idxUs <- 0 to 3 do
         if checkArray(idxUs) == us then
             for idxThem <- idxUs to (idxUs + 3) do
                 if checkArray(idxThem) == them then return idxThem - idxUs match
-                        case 0 => 3
-                        case 1 => 0
-                        case 2 => 6
+                    case 0 => 3
+                    case 1 => 0
+                    case 2 => 6
     throw new RuntimeException("Unreachable")
-}
 
-def usScore(us: RPS): Int = us.ordinal() + 1
+def usScore(us: RPS): Int = us.ordinal + 1
 
 def score(them: RPS, us: RPS): Int =  outcomeScore(them, us) + usScore(us)
 
 def calculateToken(them: RPS, outcome: Outcome): RPS =
-    val theirIdx = them.ordinal()
+    val theirIdx = them.ordinal
     outcome match
         case Win => checkArray(theirIdx + 1)
         case Lose => checkArray(theirIdx + 2)
---
+++ src/main/scala/day03/Day03.scala
@@ -0,0 +1,11 @@
+package day03
+
+import scala.io.Source;
+
+val source = Source.fromResource("day03.in")
+
+@main def main: Unit = {
+
+
+
+}
