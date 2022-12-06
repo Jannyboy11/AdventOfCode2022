38d27105b09a4a13800692ddf51e786629db08a7
Jan Boerman <janboerman95@gmail.com>
2022-12-06 07:52:22 +0100

    add day 6

--- src/main/scala/day06/Day06.scala
+++ src/main/scala/day06/Day06.scala
@@ -3,13 +3,17 @@ package day06
 import scala.io.Source;
 
 val source = Source.fromResource("day06.in")
+val input = source.mkString
+
+def findMarker(input: String, consecutiveDistinct: Int): Int =
+    input.sliding(consecutiveDistinct).indexWhere(_.toSet.size == consecutiveDistinct) + consecutiveDistinct
 
 @main def main: Unit = {
 
-    val result1 = ???
+    val result1 = findMarker(input, 4)
     println(result1)
 
-    val result2 = ???
+    val result2 = findMarker(input, 14)
     println(result2)
 
 }
