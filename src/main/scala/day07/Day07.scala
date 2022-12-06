e7067668c8e1ed369fc41ac00dc05fefa5df41f1
Jan Boerman <janboerman95@gmail.com>
2022-12-06 17:50:04 +0100

    add day 7 boilerplate

---
+++ src/main/scala/day07/Day07.scala
@@ -0,0 +1,15 @@
+package day07
+
+import scala.io.Source;
+
+val source = Source.fromResource("day07.in")
+
+@main def main: Unit = {
+
+    val result1 = ???
+    println(result1)
+
+    val result2 = ???
+    println(result2)
+
+}
